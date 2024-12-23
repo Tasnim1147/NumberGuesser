7cace10adca2c5bf4ec536c56e89fc553a2a685b
Tasnim1147 <87118828+Tasnim1147@users.noreply.github.com>
2024-12-23 17:25:02 +0000

    Fix static typing in status.py

--- python/Guess/status.py
+++ python/Guess/status.py
@@ -1,19 +1,13 @@
-"""
-WIN
-LOSS
-GREATER
-LESSER
-"""
 
 from enum import Enum
 from typing import Final
 
 class GuessState(Enum):
 
-    WIN: Final = "win"
-    LOSS: Final = "loss"
-    GREATER: Final = "greater"
-    LESSER: Final = "lesser"
+    WIN: str = "win"
+    LOSS: str = "loss"
+    GREATER: str = "greater"
+    LESSER: str = "less"
 
     def __str__(self):
         return self.value
